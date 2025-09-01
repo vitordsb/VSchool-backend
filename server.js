7f242a4a39e9388830598d091f53c6d2e39532bc
vitordsb <vitordsb2019@gmail.com>
2025-09-01 20:17:54 -0300

    Automatic Deploy

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ require("dotenv").config();
 
 const app = express();
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://vschool-five.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Conectar ao MongoDB
